d3795c168083ccf9f5f49fb407bf844f1c5c9ada
en-shungo-seino <shungo_seino@en-japan.com>
2023-08-16 19:03:42 +0900

    initial commit

---
+++ mocks/merge.js
@@ -0,0 +1,16 @@
+const fs = require("fs");
+const path = require("path");
+
+const root = "mocks/data";
+
+let apiRes = {};
+const allFile = fs.readdirSync(root, { withFileTypes: false });
+
+allFile.forEach((file) => {
+  const endpoint = path.basename(file, path.extname(file));
+  apiRes[endpoint] = JSON.parse(fs.readFileSync(root + "/" + file, "utf-8"));
+});
+
+fs.writeFileSync("mocks/db.json", JSON.stringify(apiRes), function (err) {
+  if (err) throw err;
+});
