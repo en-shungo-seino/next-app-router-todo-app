08fa752b6dbeaa82be94376cba60619f6e348a6d
en-shungo-seino <shungo_seino@en-japan.com>
2023-08-17 20:22:13 +0900

    refactor: json-server周りの環境を整えた

--- mocks/merge.js
+++ mocks/merge.js
@@ -1,16 +1,17 @@
-const fs = require("fs");
-const path = require("path");
+/* eslint-disable @typescript-eslint/no-var-requires */
+const fs = require('fs');
+const path = require('path');
 
-const root = "mocks/data";
+const root = 'mocks/data';
 
 let apiRes = {};
 const allFile = fs.readdirSync(root, { withFileTypes: false });
 
 allFile.forEach((file) => {
   const endpoint = path.basename(file, path.extname(file));
-  apiRes[endpoint] = JSON.parse(fs.readFileSync(root + "/" + file, "utf-8"));
+  apiRes[endpoint] = JSON.parse(fs.readFileSync(root + '/' + file, 'utf-8'));
 });
 
-fs.writeFileSync("mocks/db.json", JSON.stringify(apiRes), function (err) {
+fs.writeFileSync('mocks/db.json', JSON.stringify(apiRes), function (err) {
   if (err) throw err;
 });
